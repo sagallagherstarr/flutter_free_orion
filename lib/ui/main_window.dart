3afe509c5c2a648d3252539114ba6043fa529e78
Scott Gallagher-Starr <scott.gallagherstarr@gmail.com>
2023-10-22 14:55:29 -0700

    Added in the sketch of an app-level button menu

---
+++ lib/ui/main_window.dart
@@ -0,0 +1,62 @@
+import "package:flutter/material.dart";
+
+class AppMainWindow extends StatefulWidget {
+  const AppMainWindow({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<AppMainWindow> createState() => _AppMainWindowState();
+}
+
+class _AppMainWindowState extends State<AppMainWindow> {
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+        // TODO: replace with the full list of icon buttons from FreeOrion
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add_alert),
+            tooltip: 'Show Snackbar',
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('This is a ALERT')));
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.navigate_next),
+            tooltip: 'Go to the next page',
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('This is a NEXT PAGE')));
+            }
+          )
+
+        ],
+      ),
+      body: const Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Placeholder(),
+      ),
+    );
+  }
+}
